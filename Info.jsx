41bb31f77a155d999269619e26c5e327aeb8a360
Manasa-Vungarala <66659780+Manasa-Vungarala@users.noreply.github.com>
2021-06-28 10:17:21 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p1>A basic web dev React Js Bootcamp</p1>
+    </div>
+  );
+}
+
+export default Info;
